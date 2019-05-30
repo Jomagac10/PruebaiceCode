b5d7ee199d26ae1528c0ee3e4e3966c91cbddc04
Jose Manuel García Chávez <jomagac_10@hotmail.com>
2019-05-30 13:59:23 -0500

    Xcode project creation

---
+++ testIceCode/testIceCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testIceCode
+//
+//  Created by Jose Manuel García Chávez on 5/30/19.
+//  Copyright © 2019 Unam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
