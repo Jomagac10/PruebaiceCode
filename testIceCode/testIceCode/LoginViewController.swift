1bfe5cd9885880bd6d7ae4ad466874927f1abbb6
Jose Manuel García Chávez <jomagac_10@hotmail.com>
2019-05-30 21:38:27 -0500

    Add Login

---
+++ testIceCode/testIceCode/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  testIceCode
+//
+//  Created by Jose Manuel García Chávez on 5/30/19.
+//  Copyright © 2019 Unam. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
